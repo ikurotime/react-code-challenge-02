8903dec34b5e1e5b50d1de4e6a735d557e450a3d
David Huertas <davidhuertasortiz@gmail.com>
2023-10-24 19:38:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+/*
+  - TECHNICAL INTERVEW - REACT JUNIOR
+  Nos cuesta organizarnos, y por eso vamos a crear una herramienta que nos permita anotar
+  todo lo que tenemos que hacer.
+
+  Demuestra tus habilidades con React y haz una página que nos permita tomar notas de forma que
+  podamos deshacernos de ellas una vez hechas.
+  Cuando se marquen como completado, el texto de las notas se verá tachado.
+  
+  Puedes consultar información, asi como la documentación en cualquier momento
+  El resultado tiene que mostrar un input donde escribir, un botón para guardar las tareas y la lista
+  de tareas tiene que mostrarse verticalmente.
+
+  REQUISITOS
+  - Las notas se pueden guardar y se muestran en la lista[]
+  - Las notas se pueden borrar de la lista independientemente de su orden[]
+  - Las notas se pueden marcar como completado []
+  BONUS
+  - Añadir fuente personalizada al proyecto
+*/
+
+function App() {
+  return <>{/* YOUR CODE HERE */}</>
+}
+
+export default App
